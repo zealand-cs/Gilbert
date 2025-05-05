9a8bb72ff9933bba30e9682adf1ba80a11266e68
William Jelgren <william@jelgren.dk>
2025-05-05 17:50:47 +0200

    Forgot to add these files

---
+++ Gilbert/src/main/java/dk/zealandcs/gilbert/config/ThymeleafConfig.java
@@ -0,0 +1,42 @@
+package dk.zealandcs.gilbert.config;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class ThymeleafConfig {
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
---
+++ Gilbert/src/main/java/dk/zealandcs/gilbert/domain/UserRole.java
@@ -0,0 +1,14 @@
+package dk.zealandcs.gilbert.domain;
+
+public enum UserRole {
+    User(0),
+    Seller(10),
+    Employee(50),
+    Admin(100);
+
+    private final int rank;
+
+    UserRole(int rank) { this.rank = rank; }
+
+    public boolean isAtLeast(UserRole role) { return this.rank >= role.rank; }
+}
