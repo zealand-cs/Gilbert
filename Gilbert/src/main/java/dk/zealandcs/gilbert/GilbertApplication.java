cd2c0c79a7f5dd1107d432d48af4fd1f6d9eeb27
William Jelgren <william@jelgren.dk>
2025-05-05 10:30:13 +0200

    Initial project setup

---
+++ Gilbert/src/main/java/dk/zealandcs/gilbert/GilbertApplication.java
@@ -0,0 +1,13 @@
+package dk.zealandcs.gilbert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilbertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GilbertApplication.class, args);
+    }
+
+}
---
+++ Gilbert/src/test/java/dk/zealandcs/gilbert/GilbertApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.zealandcs.gilbert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GilbertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
