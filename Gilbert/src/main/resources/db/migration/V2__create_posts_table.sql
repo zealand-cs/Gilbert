2103bb50f55a1c5ee2f2cf41f9faab7dae901cfd
Magnus <magnus_petersen@outlook.com>
2025-05-08 12:13:28 +0200

    V2__create_posts_table.sql done for current version of ER

---
+++ Gilbert/src/main/resources/db/migration/V2__create_posts_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE posts
+(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    owner_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DOUBLE NOT NULL,
+    condition VARCHAR(50) NOT NULL,
+    size VARCHAR(50),
+    location VARCHAR(100),
+    status VARCHAR(50) NOT NULL,
+    image_id VARCHAR(255),
+    brands_id INT,
+    product_type_id INT,
+    date_posted_at DATE NOT NULL,
+
+    FOREIGN KEY (owner_id) REFERENCES users(id),
+    FOREIGN KEY (brands_id) REFERENCES brands(id),
+    FOREIGN KEY (product_type_id) REFERENCES product_types(id)
+)
